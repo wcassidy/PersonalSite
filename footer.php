9192f762bd3037632ed962b8144b09f815a36c5b
Wayne Cassidy <wayne.cassidy@alumni.carleton.ca>
2017-10-10 12:06:38 -0700

    Added basic page template and started working on the title bar

---
+++ footer.php
@@ -0,0 +1,6 @@
+        </div> <!-- End of container tag from header.php -->
+        <?php
+            require './scripts.html';
+        ?>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <?php
+            require './styles.html';
+        ?>
+	</head>
+    <body>
+        <div class="container-fluid">
+            <div class="row align-items-center title_bar">
+                <div class="col-1 image_right">
+                    <img src="images/MeSmall.png" class="title_image">
+                </div>
+                <div class="col-5">
+                    <h1>Wayne Cassidy BscH</h1>
+                </div>
+                <div class="col-6">
+                    <ul class="nav nav-pills nav-justified">
+                      <li class="nav-item active"><a href="#">Home</a></li>
+                      <li class="nav-item"><a href="#">Home</a></li>
+                      <li class="nav-item"><a href="#">Resume</a></li>
+                      <li class="nav-item"><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            
